499cd9f26b58fba7b2ac3bffe851cb0522bf0624
Derek Kniffin <derek.kniffin@gmail.com>
2017-03-24 11:13:24 -0400

    Add remaining tests for person_controller

--- backend/spec/controllers/person_controller_spec.rb
+++ backend/spec/controllers/person_controller_spec.rb
@@ -13,25 +13,46 @@ describe PersonController do
   end
 
   describe 'GET /person.json' do
-    let!(:person) { create(:person) }
     subject { get :index }
+    let!(:person) { create(:person) }
 
-    it 'serializes the objects' do
+    it 'serializes the people' do
       expect(PersonSerializer).to receive(:new).and_call_original
       subject
       expect(json_response).to include(hash_including("id" => person.id))
     end
   end
 
-  pending 'GET /person/:id.json' do
-    subject { get :index }
+  describe 'GET /person/:id.json' do
+    subject { get :show, params: { id: person.id } }
+    let!(:person) { create(:person) }
+
+    it 'serializes the person' do
+      expect(PersonSerializer).to receive(:new).and_call_original
+      subject
+      expect(json_response['id']).to eq(person.id)
+    end
   end
 
-  pending 'PATCH /person/:id.json' do
-    subject { get :index }
+  describe 'PATCH /person/:id.json' do
+    subject { patch :update, params: { id: person.id, person: new_params } }
+    let!(:person) { create(:person) }
+    let(:new_params) { { given_name: 'John Jacob', family_name: 'Jingleheimer-Schmidt' } }
+
+    it 'updates the person' do
+      subject
+      person.reload
+      expect(person.given_name).to eq(new_params[:given_name])
+      expect(person.family_name).to eq(new_params[:family_name])
+    end
   end
 
-  pending 'DELETE /person/:id.json' do
-    subject { get :index }
+  describe 'DELETE /person/:id.json' do
+    subject { delete :destroy, params: { id: person.id } }
+    let!(:person) { create(:person) }
+
+    it 'deletes the person' do
+      expect { subject }.to change { Person.count }.by(-1)
+    end
   end
 end
